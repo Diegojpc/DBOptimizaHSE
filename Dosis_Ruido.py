37381e16c237bd4e786498ea1c3c1b51b94ef794
Diego Jose Peña Casadiegos <dpena@ConhintecSAS.msft>
2023-12-22 12:59:53 -0500

    Modificación de modulo de ruido

--- Dosis_Ruido.py
+++ Dosis_Ruido.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import colorspacious as cs
-
+from scipy.stats import shapiro, anderson
 
 # Funcion para procesar data frame de dosis de ruido
 def dosis_de_ruido(dataframe):
@@ -14,3 +14,31 @@ def dosis_de_ruido(dataframe):
         colors = (colors * 255).astype(int)
 
         return colors[:-1]  # Excluir el último color para que haya n colores
+    
+    def GoodFit(x,a):
+        n = len(x) - x.isna().sum()
+        contador = 0
+        for i in range(n):
+            if x[i] == 0:
+                contador += 1
+
+        repetidos = np.sum(np.array(list(x.values())) >= n)
+        if n>4 and n<=5000:
+            if contador == 0 and repetidos == 0:
+                NM = 1 if shapiro(x)[1] > a else 0
+                LN = 1 if shapiro(np.log(x))[1] > a else 0
+            elif contador >= 1 and contador < n and repetidos == 0:
+                NM = 1 if shapiro(x)[1] > a else 0
+                LN = 0
+        elif n > 5000:
+            if contador == 0 and repetidos == 0:
+                NM = 1 if anderson(x, dist='norm')[1] > a else 0
+                LN = 1 if anderson(np.log(x), dist='norm')[1] > a else 0
+            elif contador >= 1 and contador < n and repetidos == 0:
+                NM = 1 if anderson(x, dist='norm')[1] > a else 0
+                LN = 0
+        else:
+            NM, LN = 0, 0
+
+        return np.array([NM, LN])    
+ 
